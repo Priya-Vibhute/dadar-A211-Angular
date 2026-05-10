a3f5875a21ff919bbbd430a486cbc5dc573d2c97
priya-vibhute <priyanka.vibhute@itvedant.com>
2026-05-10 13:34:48 +0530

    commit 10 may

--- src/app/components/serviceexample/serviceexample.component.ts
+++ src/app/components/serviceexample/serviceexample.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Product, ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-serviceexample',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './serviceexample.component.html',
   styleUrl: './serviceexample.component.css'
 })
 export class ServiceexampleComponent {
 
+  products:Product[]=[]
+  constructor(private productService:ProductService) {
+     
+  }
+
+  getProducts(){
+    this.products=this.productService.getProducts()
+  }
+
+  getProductsGreaterThan(price:number)
+  {
+    this.products=this.productService.getProductsGreaterThanPrice(price)
+  }
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+export interface Product 
+{
+    id:number,
+    name:string,
+    price:number
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  products: Product[]=[
+    { id: 1, name: 'Product 1', price: 100 },
+    { id: 2, name: 'Product 2', price: 200 },
+    { id: 3, name: 'Product 3', price: 300 },
+    { id: 4, name: 'Product 4', price: 400 },
+    { id: 5, name: 'Product 5', price: 500 },
+  ]
+  constructor() { }
+
+  getProducts()
+  {
+    return this.products;
+  }
+
+  getProductsGreaterThanPrice(price:number)
+  {
+     return this.products.filter(p=>p.price>price)
+  }
+}
