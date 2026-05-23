ab1072314311753642910f746eb285965d30b776
priya-vibhute <priyanka.vibhute@itvedant.com>
2026-05-23 17:44:22 +0530

    23 may 2

--- src/app/services/product-api.service.ts
+++ src/app/services/product-api.service.ts
@@ -1,10 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface Product 
+{
+  id:number,
+  title:string,
+  price:number,
+  description:string,
+  category:string,
+  image:string,
+  rating:{
+    rate:number,
+    count:number
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductApiService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient) { 
+
+  }
 }
